a5844d88e41f714b50be297e47086088d72e31b5
BalintHompot <hompot.balint@gmail.com>
2020-03-13 15:21:01 +0100

    example updated to class struct

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import dataset
 import ranking as rnk
 import evaluate as evl
 import numpy as np
+from LTRmodels import *
 
 
 data = dataset.get_dataset().get_data_folds()[0]
@@ -16,13 +17,12 @@ print('Number of queries in test set: %d' % data.test.num_queries())
 print('Number of documents in test set: %d' % data.test.num_docs())
 
 # initialize a random model
-random_model = np.random.uniform(size=data.num_features)
+random_model = randomModel(data.num_features)
 
-# one score for every document (1d vector in ordering of the dataset)
-all_scores = np.dot(data.train.feature_matrix, random_model)
+all_scores = random_model.score(data.train)
 
 # rank every query for all scores (1d vector ordered by query ordering in dataset)
-all_rankings, all_inverted_rankings = rnk.data_split_rank_and_invert(all_scores, data.train)
+all_rankings, all_inverted_rankings = random_model.rank(all_scores, data.train)
 
 qid = 1
 s_i, e_i = data.train.query_range(qid)
@@ -40,7 +40,7 @@ print('Scores for query %d: %s' % (qid, all_scores[s_i:e_i]))
 print('Ranking for query %d: %s' % (qid, all_rankings[s_i:e_i]))
 print('Inverted ranking for query %d: %s' % (qid, all_inverted_rankings[s_i:e_i]))
 
-validation_scores = np.dot(data.validation.feature_matrix, random_model)
+validation_scores = random_model.score(data.validation)
 print('------')
 print('Evaluation on entire validation partition.')
 results = evl.evaluate(data.validation, validation_scores, print_results=True)
